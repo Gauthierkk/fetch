792989f45624e785dece64afbcec557d720ea636
Gauthier Kelly <gauthierkk@gmail.com>
2024-11-20 12:12:30 -0600

    Delete test_api.sh

--- test_api.sh
+++
@@ -1,7 +0,0 @@
-# for each receipt in examples, send a POST request
-
-for filename in examples/*.json; do
-    curl -X POST "http://localhost:8000/receipts/process" -H "Content-Type: application/json" -d @$filename
-
-    
-done
